90f1f304f0313350992c01fc33c1309a5d13ccad
reyalpchdk <reyalp@gmail.com>
2023-03-26 17:41:29 -0700

    add header to error template, link home in header

--- components/errorpage.tsx
+++ components/errorpage.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link'
 import Head from 'next/head'
+import SiteHeader from '@/components/siteheader'
 
 type Props = {
   errno: number;
@@ -13,7 +14,8 @@ export default function ErrorPage({ errno, message}: Props) {
         <meta name="description" content="CHDK Autobuild" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <main className="flex flex-col h-screen justify-center items-center">
+      <SiteHeader />
+      <main className="flex flex-col h-[50vh] justify-end items-center">
         <div className="text-center">
           <h1 className="text-2xl mb-2">{errno} - {message}</h1>
           <Link href="/">Go home?</Link>
--- components/siteheader.tsx
+++ components/siteheader.tsx
@@ -1,12 +1,13 @@
 import Image from 'next/image'
 import logo from '../img/chdk_logo-200.png'
+import Link from 'next/link'
 
 export default function SiteHeader() {
   return (
     <header className="mb-4 bg-chdk-red1 text-white text-2xl">
       <div className="max-w-5xl mx-auto px-6 flex flex-wrap gap-x-5 py-1 items-center">
-        <Image src={logo} width={80} alt="CHDK logo" />
-        <div>CHDK AUTOBUILD</div>
+        <Link href="/"><Image src={logo} width={80} alt="CHDK logo" /></Link>
+        <Link className="hover:underline" href="/">CHDK Autobuild</Link>
         <nav className="grow flex gap-4 justify-left sm:justify-end">
           <a className="hover:underline" href="https://chdk.fandom.com/wiki/CHDK">Wiki</a>
           <a className="hover:underline" href="https://chdk.setepontos.com/index.php">Forum</a>
